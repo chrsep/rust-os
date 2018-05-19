91678154aee0548ef241f8765eea58b32b6e93e4
chrisando <chrisep8@gmail.com>
2018-05-19 12:33:05 +0700

    add more explanation on comments

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -1,8 +1,15 @@
+; make the start label global, since it will be the entry point of the
+; kernel (defined in linke.ld `ENTRY(start)`)
 global start
 
+; .text section is for executable code
 section .text
+; specifies that below is a 32 bit instruction
 bits 32
+; the label that were made global as the entrypoint
 start:
-    ; print OK to screen
+    ; move 32-bit constant 0x2f4b2f4f to 0xb8000 to print OK
     mov dword [0xb8000], 0x2f4b2f4f
+
+    ; halt command, stops the CPU until next interrupt
     hlt
--- src/arch/x86_64/multiboot_header.asm
+++ src/arch/x86_64/multiboot_header.asm
@@ -1,11 +1,21 @@
+; This is an X86 assembly file
+; .multiboot_header is used on the linker.ld for linking the binary
 section .multiboot_header
+
+; header_start and header_end is a label that marks a memory location (here 
+; it marks the beginning and end of the header in memory)
 header_start:
-    dd 0xe85250d6                   ;magic number (multiboot 2)
-    dd 0                            ;architecture 0 (protected mode i386)
-    dd header_end - header_start    ;header length
-    ;checksum
+    ; dd = define double
+    dd 0xe85250d6                   ; magic number (to indicate multiboot 2 support)
+    dd 0                            ; architecture 0 (0 idicates protected mode i386)
+    dd header_end - header_start    ; header length
+    ; checksum, 0x100000000(2^32) is used to avoid compiler warning caused by 
+    ; integer underflow in checksum
     dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
-    dw 0
-    dw 0
-    dw 8
+
+    ; dd = define word
+    ; required end tag
+    dw 0    ; type
+    dw 0    ; flags
+    dw 8    ; size
 header_end:
