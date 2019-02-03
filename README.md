35558786615d6d391b6aa312a215d489c0603beb
chrisando <chrisep8@gmail.com>
2019-02-03 13:25:14 +0700

    Add abstraction for accessing and writing char to VGA display

--- README.md
+++ README.md
@@ -1,13 +1,31 @@
 # rust-os
 
-OS built on rust.
+Repo for learning and experimenting with building
+OS using rust, I try to put a lot of comments so that the codebase
+can be easier to read for programmers that are unfamiliar with rust 
+or OS development (just like me when i started this). Based on https://os.phil-opp.com/.
 
 ## Requirements
-1. Install rust `curl https://sh.rustup.rs -sSf | sh`
-2. Install bootimage tool `cargo install bootimage`
+1. rust, install with `curl https://sh.rustup.rs -sSf | sh`
+2. bootimage tool, install with `cargo install bootimage`
 3. QEMU for running as a VM.
+4. Linux, this codebase is only tested to compile and run correctly on Linux.
 
 ## How to run
 Thanks to bootimage, running and building the OS is now a breeze
 1. Build with `bootimage build`
 2. Run with `bootimage run` to run on QEMU
+
+## Goals
+
+### Done
+- Bootable
+- Display text
+
+### Todo
+- Unit Tests
+- Integration Test
+- CPU Exceptions
+- Double Faults
+- Hardware Interrupts
+- Paging
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
-// Binary needs to be freestanding (run without underlying OS),
+// ENTRY POINT
+// GOAL:
+// Acts as the entry point for the OS, this setup all the
+// basic rules, requirements, and modules that will allow the OS to be freestanding,
+// working and bootable.
+
+
+// Binary needs to be freestanding (runs without underlying OS),
 // stdlib have dependency to OS, so we disable it
 #![no_std]
 // Freestanding binary have no access to rust's runtime and crt0.
 // crt0 is the entry point for rust programs. no_main override the
-// entry point, so crt0 and rust's runtime doesnt get called.
-// default entry point (crt0 -> rust runtime -> main)
+// entry point, so crt0 and rust's runtime doesn't get called.
+// "default entry point (crt0 -> rust runtime -> main)"
 #![no_main]
+
 use core::panic::PanicInfo;
 
 // Hello word text in byte slices
@@ -13,38 +21,33 @@ static HELLO: &[u8] = b"Hello World";
 
 // Load Bootloader
 extern crate bootloader_precompiled;
+
 // _start is the default entry point for programs in linux, so
 // we make this function to serve as our program entry point.
 //
-// no_mange used to prevent name from being mangled during compilation
+// no_mangle used to prevent name from being mangled during compilation
 // extern "C" tells the compiler to use C calling convention for this function.
 // Return type is never (`!`) aka never returns, because this function is
 // called by the bootloader directly so doesn't need to return normally. It calls exit to stop.
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // A SIMPLE HELLO WORLD TO VGA BUFFER
-    // cast 0xb8000(default location of vga buffer) to raw pointer.
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    // iterates over the HELLO bytes
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            // print into the vga buffer
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("Hello World");
 
     loop {}
 }
 
-/// Called when panic occurs
-/// A diverging function, return type is never (`!`) aka never returns
-///
-/// # Param
-/// _info: location and message from the panic
+// Handles Panic
+// A diverging function, return type is never (`!`) aka never returns
+//
+// # Param
+// _info: location and message from the panic
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
     loop {}
 }
 
+// Load vga_buffers module.
+// An abstraction that encapsulate the un-safety of using the VGA text buffer
+// through memory-mapped I/O
+mod vga_buffers;
